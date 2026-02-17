e523d1542a8dfb0358614570966dc95fe0478647
aryeefelix66-pixel <aryeefelix66@gmail.com>
2026-02-17 22:23:53 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -113,12 +113,15 @@ void createSession() {
 void showMenu();
 
 void showMenu() {
-    cout << "\n===== Digital Attendance System =====" << endl;
-    cout << "1. Add Student" << endl;
-    cout << "2. View Students" << endl;
-    cout << "3. Mark Attendance" << endl;
-    cout << "4. Exit" << endl;
-    cout << "Enter your choice: ";
+ cout << "\n===== Digital Attendance System =====\n";
+cout << "1. Add Student\n";
+cout << "2. View Students\n";
+cout << "3. Search Student\n";
+cout << "4. Create Attendance Session\n";
+cout << "5. Save Students\n";
+cout << "6. Exit\n";
+cout << "Enter choice: ";
+   
 }
 
 
